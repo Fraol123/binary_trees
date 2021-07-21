40d0b963f5dad86738fbbaf8e47a4b8c29c03edc
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-07-21 13:07:45 +0000

    leaf added

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,6 +1,17 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ *
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, otherwise 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	return ((!node->left && !node->right) ? 1 : 0);
+}
+/**
  * binary_tree_leaves- function that counts the
  *                  number of leaves in a binary tree
  *
