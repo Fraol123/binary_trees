c1d07fbd942eb3b79a0988d2931b06561c7f03d5
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-07-21 13:11:38 +0000

    betty iso

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,19 +1,32 @@
 #include "binary_trees.h"
+
 /**
-* binary_tree_nodes-function that counts the
-*nodes with at least 1 child in a binary tree
-*
-* @tree: a ptr to the root node of the tree to count the nodes with children
-*
-* Return: number of nodes with at least 1 child   * in a binary tree
-*/
+ * binary_tree_is_leaf - checks if a node is a leaf
+ *
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, otherwise 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	return ((!node->left && !node->right) ? 1 : 0);
+}
+
+/**
+ * binary_tree_nodes-function that counts the
+ *nodes with at least 1 child in a binary tree
+ *
+ * @tree: a ptr to the root node of the tree to count the nodes with children
+ *
+ * Return: number of nodes with at least 1 child   * in a binary tree
+ */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree == NULL || binary_tree_is_leaf(tree))
-		return (0);
-
 	size_t left = 0, right = 0;
+
+	if (tree == NULL || binary_tree_is_leaf(tree))
+		return (0);	
 
 	left = binary_tree_nodes(tree->left);
 
