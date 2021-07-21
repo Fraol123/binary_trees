e7cc986412c4b8e5d649a68b67c8f3e16edebe53
JudgeFudge19 <dremukare@gmail.com>
2021-07-21 11:45:18 +0000

    fixed betty errors"

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,12 +12,12 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-    
+
 	if  (tree->right == NULL && tree->left == NULL)
-		return (1);   
+		return (1);
 
 	if ((tree->left) && (tree->right))
-		return binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right);
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
 	return (0);
 }
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,7 +16,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (1);
 
 	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
-		return binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right);
+		return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right));
 
 	return (0);
 }
