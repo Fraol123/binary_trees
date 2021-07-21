8e61f7dd51b4ef349f70d474929f5b87699fee2a
JudgeFudge19 <dremukare@gmail.com>
2021-07-21 12:59:22 +0000

    fixed variable declaration

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,14 +10,16 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t depth_left = 0, depth_right = 0, height = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t depth_left = binary_tree_height(tree->left);
+	depth_left = binary_tree_height(tree->left);
 
-	size_t depth_right = binary_tree_height(tree->right);
+	depth_right = binary_tree_height(tree->right);
 
-	size_t height = ((depth_left > depth_right) ? depth_left : depth_right) + 1;
+	height = ((depth_left > depth_right) ? depth_left : depth_right) + 1;
 
 	return (height);
 }
