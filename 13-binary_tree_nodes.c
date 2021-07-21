7a05c3c4f2f49b1ebc967e465a6a364707063010
JudgeFudge19 <dremukare@gmail.com>
2021-07-21 13:13:34 +0000

    betty error

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -26,7 +26,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	size_t left = 0, right = 0;
 
 	if (tree == NULL || binary_tree_is_leaf(tree))
-		return (0);	
+		return (0);
 
 	left = binary_tree_nodes(tree->left);
 
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,30 @@
 #include "binary_trees.h"
 
+/**                                                                             
+* binary_tree_height - measures the height of a binary tree                     
+*                                                                               
+* @tree: pointer to the root node of the tree to measure the height             
+*                                                                               
+* Return: height or return 0 if tree is NULL                                    
+*/                                                                              
+                                                                                
+size_t binary_tree_height(const binary_tree_t *tree)                            
+{                                                                               
+    size_t depth_left, depth_right, height;                                     
+                                                                                
+    if (tree == NULL)                                                           
+        return (0);                                                             
+                                                                                
+    depth_left = binary_tree_height(tree->left);                                
+                                                                                
+    depth_right = binary_tree_height(tree->right);                              
+                                                                                
+    height = ((depth_left > depth_right) ? depth_left : depth_right) + 1;       
+                                                                                
+    return (height);                                                            
+}                                                                               
+                                                                                
+
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  *
