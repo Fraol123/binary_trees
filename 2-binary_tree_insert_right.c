6f16dfa19faeeca868a3e58a5b29ee2ce13a8baf
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-07-21 11:47:52 +0000

    betty error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -21,5 +21,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		new_right_node->right->parent = new_right_node;
 
 	parent->right = new_right_node;
-	return new_right_node;
+	return (new_right_node);
 }
--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,9 +10,9 @@
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
-    return;
+		return;
 
-  binary_tree_delete(tree->left);
-  binary_tree_delete(tree->right);
-  free(tree);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
