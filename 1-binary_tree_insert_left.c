88dea086c98da4da6fc05c089136a84562a9d342
JudgeFudge19 <dremukare@gmail.com>
2021-07-21 12:56:59 +0000

    added NULL initialization to variable declaration

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,7 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_left_node;
+	binary_tree_t *new_left_node = NULL;
 
 	if (parent == NULL)
 		return (NULL);
