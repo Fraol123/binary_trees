6a8332780fc5784e70319588e873a36a31ec92d0
JudgeFudge19 <dremukare@gmail.com>
2021-07-21 11:34:01 +0000

    fixed betty errors

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,12 +9,12 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-  size_t left = 0, right = 0;
-  
-	if(tree == NULL)
-    return (0);
+	size_t left = 0, right = 0;
 
-  left = binary_tree_size(tree->left);
-  right = binary_tree_size(tree->right);
-  return (1 + left + right);
+	if (tree == NULL)
+		return (0);
+
+	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
+	return (1 + left + right);
 }
