b69f1f7e8d7cbdd87106ed32341046ad6b83c6cd
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-07-23 12:05:52 +0000

    malloc error not handled

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,27 +1,5 @@
 #include "binary_trees.h"
-/**
- * binary_tree_node - creates a new binary tree node
- *
- *@parent: pointer to parent
- *@value: value in binary tree
- *
- * Return: pointer to binary tree
- */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new_node = NULL;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->right = NULL;
-	new_node->left = NULL;
-
-	return (new_node);
-}
 /**
 *binary_tree_insert_left - inserts a node as the left-child of another node
 *
